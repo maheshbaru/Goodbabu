68ea6527b3a9e30c74df5f91ee53e4b8c16d5edc
maheshbaru <90108219+maheshbaru@users.noreply.github.com>
2021-09-04 23:44:26 +0530

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -25,9 +25,3 @@ echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-#install ngrok
-brew cask install ngrok
-
-#configure ngrok and start it
-ngrok authtoken $3
-ngrok tcp 5900 &
